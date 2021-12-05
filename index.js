a9137e11b56640e7d698462724bb4cf76e58b6b4
Nikhil-Malviya <>
2021-12-05 13:40:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var readlinesync=require("readline-sync");
+var score=0;
+var username=readlinesync.question("whats your name? ");
+console.log("welcome " + username + " to do know nikhil? ");
+
+function play (question,answer){
+  var userans=readlinesync.question(question);
+  if (userans.toUpperCase()===answer.toUpperCase()){
+    console.log("your right");
+    score=score+1
+  }else{
+    console.log("your wrong")
+  }
+
+  console.log("current score",score);
+  console.log("_____")
+}
+
+var highScore=[{
+  'Bipin':3,
+},
+{
+  'Manoj':3
+}];
+
+var questions=[{
+  question:"Where do i live? ",
+  answer:"Indore"
+},{
+  question:"Where do i work? ",
+  answer:"Microsoft"
+}];
+for (var i=0;i<questions.length;i++){
+  var currentquestion=questions[i];
+  play(currentquestion.question,currentquestion.answer)
+}
+console.log("yaa, you scored",score);
+console.log("highScore" , highScore[0],highScore[1])
